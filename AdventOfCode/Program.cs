067b197fb2ad63ecab9516ac2d2a50b15bc30433
Chris Caskey <christopher.caskey@saabgroup.com>
2021-12-03 19:54:35 +0000

    Add an application chooser
    Add an application chooser (based off Jon Skeet's DemoUtil) to allow
    challenges over various years to be added and ran.

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -6,7 +6,7 @@ namespace AdventOfCode
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ApplicationChooser.Run();
         }
     }
 }
