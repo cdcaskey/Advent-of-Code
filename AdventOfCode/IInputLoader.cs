cbcf88224fb50b9db7889e62aae9b0cc6235976f
Chris Caskey <christopher.caskey@saabgroup.com>
2021-12-06 12:34:02 +0000

    Create a basic input loader class

---
+++ AdventOfCode/IInputLoader.cs
@@ -0,0 +1,7 @@
+﻿namespace AdventOfCode
+{
+    public interface IInputLoader
+    {
+        string LoadInput(string filePath);
+    }
+}
---
+++ AdventOfCode/InputLoader.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+
+namespace AdventOfCode
+{
+    public class InputLoader : IInputLoader
+    {
+        public string LoadInput(string filePath)
+        {
+            using var reader = new StreamReader(filePath);
+            return reader.ReadToEnd();
+        }
+    }
+}
