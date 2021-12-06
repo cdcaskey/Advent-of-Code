167f7e55c272815ec4f304f2d88889c239c6aa94
Chris Caskey <cdcaskey@users.noreply.github.com>
2021-12-06 18:31:21 +0000

    Complete 2021 Day 6

---
+++ AdventOfCode.Tests/2021/Day06Tests.cs
@@ -0,0 +1,49 @@
+﻿using AdventOfCode._2021;
+using Moq;
+using NUnit.Framework;
+using Shouldly;
+
+namespace AdventOfCode.Tests._2021
+{
+    [TestFixture]
+    public class Day06Tests
+    {
+        private Mock<IInputLoader> loader;
+
+        [SetUp]
+        public void Setup()
+        {
+            loader = new Mock<IInputLoader>();
+            loader.Setup(x => x.LoadArray<int>(It.IsAny<string>(), It.IsAny<string>()))
+                  .Returns(new int [] { 3, 4, 3, 1, 2 });
+        }
+
+        [Test]
+        public void PartA()
+        {
+            // Arrange
+            CreateSut();
+
+            // Act
+            var result = Day06.PartA();
+
+            // Assert
+            result.ShouldBe(5934);
+        }
+
+        [Test]
+        public void PartB()
+        {
+            // Arrange
+            CreateSut();
+
+            // Act
+            var result = Day06.PartB();
+
+            // Assert
+            result.ShouldBe(26984457539);
+        }
+
+        private Day06 CreateSut() => new Day06(loader.Object);
+    }
+}
---
+++ AdventOfCode/2021/Day06.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode._2021
+{
+    public class Day06 : CodeChallenge
+    {
+        private const string inputLocation = "Inputs\\2021\\Day06.txt";
+
+        public Day06(IInputLoader loader) : base(loader) { }
+
+        public static void Main()
+        {
+            while (true)
+            {
+                Console.Write("Part A or B? (or 'q' to quit): ");
+
+                switch (Console.ReadLine().ToUpper())
+                {
+                    case "A":
+                        Console.WriteLine($"Result: {PartA()}");
+                        break;
+
+                    case "B":
+                        Console.WriteLine($"Result: {PartB()}");
+                        break;
+
+                    case "Q":
+                        return;
+
+                    default:
+                        Console.Write("Invalid Input - ");
+                        break;
+                }
+            }
+        }
+
+        public static long PartA() => Simulate(80);
+
+        public static long PartB() => Simulate(256);
+
+        private static long Simulate(int days)
+        {
+            var fish = new Dictionary<int, long>();
+            for (var i = 0; i <= 8; i++)
+            {
+                fish[i] = 0;
+            }
+
+            var startFish = inputLoader.LoadArray<int>(inputLocation, ",");
+            foreach (var f in startFish)
+            {
+                fish[f]++;
+            }
+
+            for (var day = 0; day < days; day++)
+            {
+                var fishToAdd = fish[0];
+                for (var i = 1; i <= 8; i++)
+                {
+                    fish[i - 1] = fish[i];
+                }
+
+                fish[6] += fishToAdd;
+                fish[8] = fishToAdd;
+            }
+
+            return fish.Sum(x => x.Value);
+        }
+    }
+}
