4bf7e2c59fd44f93080c5d0862a706cf40fcb1ae
Chris Caskey <cdcaskey@users.noreply.github.com>
2021-12-06 17:37:04 +0000

    Complete 2021 Day 5

---
+++ AdventOfCode.Tests/2021/Day05Tests.cs
@@ -0,0 +1,58 @@
+﻿using AdventOfCode._2021;
+using Moq;
+using NUnit.Framework;
+using Shouldly;
+
+namespace AdventOfCode.Tests._2021
+{
+    [TestFixture]
+    public class Day05Tests
+    {
+        private Mock<IInputLoader> loader;
+
+        [SetUp]
+        public void Setup()
+        {
+            loader = new Mock<IInputLoader>();
+            loader.Setup(x => x.LoadInput(It.IsAny<string>()))
+                  .Returns(@"0,9 -> 5,9
+8,0 -> 0,8
+9,4 -> 3,4
+2,2 -> 2,1
+7,0 -> 7,4
+6,4 -> 2,0
+0,9 -> 2,9
+3,4 -> 1,4
+0,0 -> 8,8
+5,5 -> 8,2");
+        }
+
+        [Test]
+        public void PartA()
+        {
+            // Arrange
+            CreateSut();
+
+            // Act
+            var result = Day05.PartA();
+
+            // Assert
+            result.ShouldBe(5);
+        }
+
+        [Test]
+        public void PartB()
+        {
+            // Arrange
+            CreateSut();
+
+            // Act
+            var result = Day05.PartB();
+
+            // Assert
+            result.ShouldBe(12);
+        }
+
+        private Day05 CreateSut() => new Day05(loader.Object);
+    }
+}
---
+++ AdventOfCode/2021/Day05.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode._2021
+{
+    public class Day05 : CodeChallenge
+    {
+        private const string inputLocation = "Inputs\\2021\\Day05.txt";
+
+        public Day05(IInputLoader loader) : base(loader) { }
+
+        public static void Main()
+        {
+            while (true)
+            {
+                Console.Write("Part A or B? (or 'q' to quit): ");
+
+                switch(Console.ReadLine().ToUpper())
+                {
+                    case "A":
+                        Console.WriteLine($"Result: {PartA()}");
+                        break;
+
+                    case "B":
+                        Console.WriteLine($"Result: {PartB()}");
+                        break;
+
+                    case "Q":
+                        return;
+
+                    default:
+                        Console.Write("Invalid Input - ");
+                        break;
+                }
+            }
+        }
+
+        public static int PartA()
+        {
+            var lines = ParseLines(out var maxX, out var maxY);
+            var grid = new int[maxX, maxY];
+
+            foreach (var line in lines)
+            {
+                if (!line.IsHorizontalOrVertical())
+                {
+                    continue;
+                }
+
+                if (line.StartX > line.EndX || line.StartY > line.EndY)
+                {
+                    line.Flip();
+                }
+
+                for (var x = line.StartX; x <= line.EndX; x++)
+                {
+                    for (var y = line.StartY; y <= line.EndY; y++)
+                    {
+                        grid[x, y]++;
+                    }
+                }
+            }
+
+            var overlaps = 0;
+            foreach (var cell in grid)
+            {
+                if (cell > 1)
+                {
+                    overlaps++;
+                }
+            }
+
+            return overlaps;
+        }
+
+        public static int PartB()
+        {
+            var lines = ParseLines(out var maxX, out var maxY);
+            var grid = new int[maxX, maxY];
+
+            foreach (var line in lines)
+            {
+                if (line.IsHorizontalOrVertical())
+                {
+                    if (line.StartX > line.EndX || line.StartY > line.EndY)
+                    {
+                        line.Flip();
+                    }
+
+                    for (var x = line.StartX; x <= line.EndX; x++)
+                    {
+                        for (var y = line.StartY; y <= line.EndY; y++)
+                        {
+                            grid[x, y]++;
+                        }
+                    }
+                }
+                else if (line.Is45DegreeDiagonal())
+                {
+                    if (line.StartX > line.EndX)
+                    {
+                        line.Flip();
+                    }
+
+                    if (line.StartY <= line.EndY)
+                    {
+                        for (int x = line.StartX, y = line.StartY; y <= line.EndY; x++, y++)
+                        {
+                            grid[x, y]++;
+                        }
+                    }
+                    else
+                    {
+                        for (int x = line.StartX, y = line.StartY; y >= line.EndY; x++, y--)
+                        {
+                            grid[x, y]++;
+                        }
+                    }
+                }
+            }
+
+            var overlaps = 0;
+            foreach (var cell in grid)
+            {
+                if (cell > 1)
+                {
+                    overlaps++;
+                }
+            }
+
+            return overlaps;
+        }
+
+        private static List<Line> ParseLines(out int maxX, out int maxY)
+        {
+            var input = inputLoader.LoadInput(inputLocation);
+            var lines = new List<Line>();
+            maxX = 0;
+            maxY = 0;
+            foreach (var line in input.Split("\r\n"))
+            {
+                var regex = Regex.Match(line, @"(\d+),(\d+) -> (\d+),(\d+)");
+
+                var startX = int.Parse(regex.Groups[1].Value);
+                var startY = int.Parse(regex.Groups[2].Value);
+                var endX = int.Parse(regex.Groups[3].Value);
+                var endY = int.Parse(regex.Groups[4].Value);
+
+                maxX = Math.Max(startX + 1, maxX);
+                maxX = Math.Max(endX + 1, maxX);
+                maxY = Math.Max(startY + 1, maxY);
+                maxY = Math.Max(endY + 1, maxY);
+
+                lines.Add(new Line(startX, startY, endX, endY));
+            }
+
+            return lines;
+        }
+
+
+
+        private class Line
+        {
+            public int StartX { get; set; }
+
+            public int StartY { get; set; }
+
+            public int EndX { get; set; }
+
+            public int EndY { get; set; }
+
+            public Line(int startX, int startY, int endX, int endY)
+            {
+                StartX = startX;
+                StartY = startY;
+                EndX = endX;
+                EndY = endY;
+            }
+
+            public bool IsHorizontalOrVertical() => (StartX - EndX == 0) || (StartY - EndY == 0);
+
+            public bool Is45DegreeDiagonal() => Math.Abs(StartX - EndX) == Math.Abs(StartY - EndY);
+
+            public void Flip()
+            {
+                var tempX = EndX;
+                var tempY = EndY;
+
+                EndX = StartX;
+                EndY = StartY;
+
+                StartX = tempX;
+                StartY = tempY;
+            }
+        }
+    }
+}
