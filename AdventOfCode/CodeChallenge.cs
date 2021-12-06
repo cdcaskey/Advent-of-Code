75987b74a26d28062cfe6f95b082e50bff156ae8
Chris Caskey <christopher.caskey@saabgroup.com>
2021-12-06 12:57:07 +0000

    Add a base class for challenges

---
+++ AdventOfCode/CodeChallenge.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    public abstract class CodeChallenge
+    {
+        protected static IInputLoader inputLoader;
+
+        static CodeChallenge()
+        {
+            inputLoader = new InputLoader();
+        }
+
+        public CodeChallenge(IInputLoader loader)
+        {
+            inputLoader = loader;
+        }
+    }
+}
