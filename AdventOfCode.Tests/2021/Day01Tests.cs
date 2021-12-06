9217b124ad2f064d238cad8aefd06fed9d26d2be
Chris Caskey <cdcaskey@users.noreply.github.com>
2021-12-06 13:39:32 +0000

    Complete 2021 Day 1

---
+++ AdventOfCode.Tests/2021/Day01Tests.cs
@@ -0,0 +1,49 @@
+﻿using AdventOfCode._2021;
+using Moq;
+using NUnit.Framework;
+using Shouldly;
+
+namespace AdventOfCode.Tests._2021
+{
+    [TestFixture]
+    public class Day01Tests
+    {
+        private Mock<IInputLoader> loader;
+
+        [SetUp]
+        public void Setup()
+        {
+            loader = new Mock<IInputLoader>();
+            loader.Setup(x => x.LoadArray<int>(It.IsAny<string>(), It.IsAny<string>()))
+                  .Returns(new[] { 199, 200, 208, 210, 200, 207, 240, 269, 260, 263 });
+        }
+
+        [Test]
+        public void PartA()
+        {
+            // Arrange
+            CreateSut();
+
+            // Act
+            var result = Day01.PartA();
+
+            // Assert
+            result.ShouldBe(7);
+        }
+
+        [Test]
+        public void PartB()
+        {
+            // Arrange
+            CreateSut();
+
+            // Act
+            var result = Day01.PartB();
+
+            // Assert
+            result.ShouldBe(5);
+        }
+
+        private Day01 CreateSut() => new Day01(loader.Object);
+    }
+}
