1c75a8e18b0f820a2c07588c8a88bca4085d040d
Chris Caskey <cdcaskey@users.noreply.github.com>
2021-12-07 15:33:59 +0000

    Complete 2020 Day 3

---
+++ AdventOfCode.Tests/2020/Day03Tests.cs
@@ -0,0 +1,62 @@
+﻿using AdventOfCode._2020;
+using Moq;
+using NUnit.Framework;
+using Shouldly;
+
+namespace AdventOfCode.Tests._2020
+{
+    [TestFixture]
+    public class Day03Tests
+    {
+        private Mock<IInputLoader> loader;
+
+        [SetUp]
+        public void Setup()
+        {
+            loader = new Mock<IInputLoader>();
+            loader.Setup(x => x.LoadArray<string>(It.IsAny<string>(), It.IsAny<string>()))
+                  .Returns(new string[]
+                  {
+                      "..##.......",
+                      "#...#...#..",
+                      ".#....#..#.",
+                      "..#.#...#.#",
+                      ".#...##..#.",
+                      "..#.##.....",
+                      ".#.#.#....#",
+                      ".#........#",
+                      "#.##...#...",
+                      "#...##....#",
+                      ".#..#...#.#"
+                  });
+        }
+
+        [Test]
+        public void PartA()
+        {
+            // Arrange
+            var sut = CreateSut();
+
+            // Act
+            var result = sut.PartA();
+
+            // Assert
+            result.ShouldBe(7);
+        }
+
+        [Test]
+        public void PartB()
+        {
+            // Arrange
+            var sut = CreateSut();
+
+            // Act
+            var result = sut.PartB();
+
+            // Assert
+            result.ShouldBe(336);
+        }
+
+        private Day03 CreateSut() => new Day03(loader.Object);
+    }
+}
---
+++ AdventOfCode/2020/Day03.cs
@@ -0,0 +1,52 @@
+﻿namespace AdventOfCode._2020
+{
+    public class Day03 : CodeChallenge
+    {
+        public Day03(IInputLoader loader) : base(loader) { }
+
+        public override int Year => 2020;
+
+        public override int Day => 3;
+
+        public override long PartA()
+        {
+            var slope = inputLoader.LoadArray<string>(inputLocation);
+
+            return CalculateTrees(slope, 3, 1);
+        }
+
+        public override long PartB()
+        {
+            var slope = inputLoader.LoadArray<string>(inputLocation);
+
+            var trees = 1;
+
+            trees *= CalculateTrees(slope, 1, 1);
+            trees *= CalculateTrees(slope, 3, 1);
+            trees *= CalculateTrees(slope, 5, 1);
+            trees *= CalculateTrees(slope, 7, 1);
+            trees *= CalculateTrees(slope, 1, 2);
+
+            return trees;
+        }
+
+        private int CalculateTrees(string[] slope, int xStep, int yStep)
+        {
+            var trees = 0;
+            for (int x = 0, y = 0; y < slope.Length; x += xStep, y += yStep)
+            {
+                if (x >= slope[y].Length)
+                {
+                    x -= slope[y].Length;
+                }
+
+                if (slope[y][x] == '#')
+                {
+                    trees++;
+                }
+            }
+
+            return trees;
+        }
+    }
+}
