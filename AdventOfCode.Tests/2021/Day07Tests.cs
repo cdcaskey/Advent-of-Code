2c1005a00eedc16e805e845c39de59a7a94996b2
Chris Caskey <cdcaskey@users.noreply.github.com>
2021-12-07 11:26:30 +0000

    Complete 2021 Day 7

---
+++ AdventOfCode.Tests/2021/Day07Tests.cs
@@ -0,0 +1,49 @@
+﻿using AdventOfCode._2021;
+using Moq;
+using NUnit.Framework;
+using Shouldly;
+
+namespace AdventOfCode.Tests._2021
+{
+    [TestFixture]
+    public class Day07Tests
+    {
+        private Mock<IInputLoader> loader;
+
+        [SetUp]
+        public void Setup()
+        {
+            loader = new Mock<IInputLoader>();
+            loader.Setup(x => x.LoadArray<int>(It.IsAny<string>(), It.IsAny<string>()))
+                  .Returns(new int [] { 16, 1, 2, 0, 4, 2, 7, 1, 2, 14 });
+        }
+
+        [Test]
+        public void PartA()
+        {
+            // Arrange
+            var sut = CreateSut();
+
+            // Act
+            var result = sut.PartA();
+
+            // Assert
+            result.ShouldBe(37);
+        }
+
+        [Test]
+        public void PartB()
+        {
+            // Arrange
+            var sut = CreateSut();
+
+            // Act
+            var result = sut.PartB();
+
+            // Assert
+            result.ShouldBe(168);
+        }
+
+        private Day07 CreateSut() => new Day07(loader.Object);
+    }
+}
---
+++ AdventOfCode/2021/Day07.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode._2021
+{
+    public class Day07 : CodeChallenge
+    {
+        public Day07(IInputLoader loader) : base(loader) { }
+
+        public override int Year => 2021;
+
+        public override int Day => 7;
+
+        public override long PartA()
+        {
+            var input = inputLoader.LoadArray<int>(inputLocation, ",");
+
+            var crabs = new Dictionary<int, int>();
+            foreach (var crab in input)
+            {
+                if (crabs.TryGetValue(crab, out var existingNumber))
+                {
+                    crabs[crab]++;
+                }
+                else
+                {
+                    crabs[crab] = 1;
+                }
+            }
+
+            var minFuel = int.MaxValue;
+            for (var target = crabs.Keys.Min(); target <= crabs.Keys.Max(); target++)
+            {
+                var fuelToReachTarget = 0;
+                foreach (var crab in crabs)
+                {
+                    fuelToReachTarget += Math.Abs(crab.Key - target) * crab.Value;
+                }
+
+                if (fuelToReachTarget < minFuel)
+                {
+                    minFuel = fuelToReachTarget;
+                }
+            }
+
+            return minFuel;
+        }
+
+        public override long PartB()
+        {
+            var input = inputLoader.LoadArray<int>(inputLocation, ",");
+
+            var crabs = new Dictionary<int, int>();
+            foreach (var crabGroup in input)
+            {
+                if (crabs.TryGetValue(crabGroup, out var existingNumber))
+                {
+                    crabs[crabGroup]++;
+                }
+                else
+                {
+                    crabs[crabGroup] = 1;
+                }
+            }
+
+            var minFuel = int.MaxValue;
+            for (var target = crabs.Keys.Min(); target <= crabs.Keys.Max(); target++)
+            {
+                var fuelToReachTarget = 0;
+                foreach (var crabGroup in crabs)
+                {
+                    var distanceToTarget = Math.Abs(crabGroup.Key - target);
+                    var fuelForDistance = distanceToTarget * (distanceToTarget + 1) / 2;
+
+                    fuelToReachTarget += fuelForDistance * crabGroup.Value;
+                }
+
+                if (fuelToReachTarget < minFuel)
+                {
+                    minFuel = fuelToReachTarget;
+                }
+            }
+
+            return minFuel;
+        }
+    }
+}
