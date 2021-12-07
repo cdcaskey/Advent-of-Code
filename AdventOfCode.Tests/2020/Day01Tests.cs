d5dc624f579da6c635e8eac66f54f6ce593b2773
Chris Caskey <cdcaskey@users.noreply.github.com>
2021-12-07 02:15:40 +0000

    Complete 2020 Day 1

---
+++ AdventOfCode.Tests/2020/Day01Tests.cs
@@ -0,0 +1,49 @@
+﻿using AdventOfCode._2020;
+using Moq;
+using NUnit.Framework;
+using Shouldly;
+
+namespace AdventOfCode.Tests._2020
+{
+    [TestFixture]
+    public class Day01Tests
+    {
+        private Mock<IInputLoader> loader;
+
+        [SetUp]
+        public void Setup()
+        {
+            loader = new Mock<IInputLoader>();
+            loader.Setup(x => x.LoadArray<int>(It.IsAny<string>(), It.IsAny<string>()))
+                  .Returns(new[] { 1721, 979, 366, 299, 675, 1456 });
+        }
+
+        [Test]
+        public void PartA()
+        {
+            // Arrange
+            var sut = CreateSut();
+
+            // Act
+            var result = sut.PartA();
+
+            // Assert
+            result.ShouldBe(514579);
+        }
+
+        [Test]
+        public void PartB()
+        {
+            // Arrange
+            var sut = CreateSut();
+
+            // Act
+            var result = sut.PartB();
+
+            // Assert
+            result.ShouldBe(241861950);
+        }
+
+        private Day01 CreateSut() => new Day01(loader.Object);
+    }
+}
