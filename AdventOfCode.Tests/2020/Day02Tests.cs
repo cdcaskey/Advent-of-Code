a1553e6c8e23b5a2b49c6c997456f271be54d640
Chris Caskey <cdcaskey@users.noreply.github.com>
2021-12-07 15:11:39 +0000

    Complete 2020 Day 2

---
+++ AdventOfCode.Tests/2020/Day02Tests.cs
@@ -0,0 +1,54 @@
+﻿using AdventOfCode._2020;
+using Moq;
+using NUnit.Framework;
+using Shouldly;
+
+namespace AdventOfCode.Tests._2020
+{
+    [TestFixture]
+    public class Day02Tests
+    {
+        private Mock<IInputLoader> loader;
+
+        [SetUp]
+        public void Setup()
+        {
+            loader = new Mock<IInputLoader>();
+            loader.Setup(x => x.LoadArray<string>(It.IsAny<string>(), It.IsAny<string>()))
+                  .Returns(new string[]
+                  {
+                      "1-3 a: abcde",
+                      "1-3 b: cdefg",
+                      "2-9 c: ccccccccc"
+                  });
+        }
+
+        [Test]
+        public void PartA()
+        {
+            // Arrange
+            var sut = CreateSut();
+
+            // Act
+            var result = sut.PartA();
+
+            // Assert
+            result.ShouldBe(2);
+        }
+
+        [Test]
+        public void PartB()
+        {
+            // Arrange
+            var sut = CreateSut();
+
+            // Act
+            var result = sut.PartB();
+
+            // Assert
+            result.ShouldBe(1);
+        }
+
+        private Day02 CreateSut() => new Day02(loader.Object);
+    }
+}
---
+++ AdventOfCode/2020/Day02.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode._2020
+{
+    public class Day02 : CodeChallenge
+    {
+        public Day02(IInputLoader loader) : base(loader) { }
+
+        public override int Year => 2020;
+
+        public override int Day => 2;
+
+        public override long PartA()
+        {
+            var input = inputLoader.LoadArray<string>(inputLocation);
+
+            var validPasswords = 0;
+            foreach (var line in input)
+            {
+                var regex = Regex.Match(line, @"(\d+)-(\d+) ([a-z]): ([a-z]+)");
+
+                var minTimes = int.Parse(regex.Groups[1].Value);
+                var maxTimes = int.Parse(regex.Groups[2].Value);
+                var letter = regex.Groups[3].Value[0];
+                var password = regex.Groups[4].Value;
+
+                var letterOccurances = password.Count(c => c == letter);
+
+                if (letterOccurances >= minTimes && letterOccurances <= maxTimes)
+                {
+                    validPasswords++;
+                }
+            }
+
+            return validPasswords;
+        }
+
+        public override long PartB()
+        {
+            var input = inputLoader.LoadArray<string>(inputLocation);
+
+            var validPasswords = 0;
+            foreach (var line in input)
+            {
+                var regex = Regex.Match(line, @"(\d+)-(\d+) ([a-z]): ([a-z]+)");
+
+                var pos1 = int.Parse(regex.Groups[1].Value) - 1;
+                var pos2 = int.Parse(regex.Groups[2].Value) - 1;
+                var letter = regex.Groups[3].Value[0];
+                var password = regex.Groups[4].Value;
+
+                if (password[pos1] == letter ^ password[pos2] == letter)
+                {
+                    validPasswords++;
+                }
+            }
+
+            return validPasswords;
+        }
+    }
+}
