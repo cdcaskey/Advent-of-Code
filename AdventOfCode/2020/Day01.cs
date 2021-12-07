41a121e77f7e1062b96642a9f178d2e9cc472547
Chris Caskey <cdcaskey@users.noreply.github.com>
2021-12-07 02:15:40 +0000

    Complete 2020 Day 1

---
+++ AdventOfCode/2020/Day01.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace AdventOfCode._2020
+{
+    public class Day01 : CodeChallenge
+    {
+        public Day01(IInputLoader loader) : base(loader) { }
+
+        public override int Year => 2020;
+
+        public override int Day => 1;
+
+        public override long PartA()
+        {
+            var input = inputLoader.LoadArray<int>(inputLocation);
+
+            for (var i = 0; i < input.Length; i++)
+            {
+                for (var j = 0; j < input.Length; j++)
+                {
+                    if (i == j)
+                    {
+                        continue;
+                    }
+                    if (input[i] + input[j] == 2020)
+                    {
+                        return input[i] * input[j];
+                    }
+                }
+            }
+
+            throw new Exception("Couldn't find 2 values that add to 2020.");
+        }
+
+        public override long PartB()
+        {
+            var input = inputLoader.LoadArray<int>(inputLocation);
+
+            for (var i = 0; i < input.Length; i++)
+            {
+                for (var j = 0; j < input.Length; j++)
+                {
+                    for (var k = 0; k < input.Length; k++)
+                    {
+                        if (input[i] == j || i == k || j == k)
+                        {
+                            continue;
+                        }
+                        if (input[i] + input[j] + input[k] == 2020)
+                        {
+                            return input[i] * input[j] * input[k];
+                        }
+                    }
+                }
+            }
+
+            throw new Exception("Couldn't find 3 values that add to 2020.");
+        }
+    }
+}
