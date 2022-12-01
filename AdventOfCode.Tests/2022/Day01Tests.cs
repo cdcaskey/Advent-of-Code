3db696279127052c1e34476a912f6383128718b2
Chris Caskey <cdcaskey@users.noreply.github.com>
2022-12-01 11:37:03 +0000

    Complete 2022 Day 1

---
+++ AdventOfCode.Tests/2022/Day01Tests.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using AdventOfCode._2022;
+using Moq;
+using NUnit.Framework;
+using Shouldly;
+
+namespace AdventOfCode.Tests._2022
+{
+    [TestFixture]
+    public class Day01Tests
+    {
+        private Mock<IInputLoader> loader;
+
+        [SetUp]
+        public void Setup()
+        {
+            loader = new Mock<IInputLoader>();
+            loader.Setup(x => x.LoadListOfArrays<int>(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()))
+                  .Returns(new List<int[]>()
+                  {
+                      new int[] { 1000, 2000, 3000},
+                      new int[] { 4000 },
+                      new int[] { 5000, 6000 },
+                      new int[] { 7000, 8000, 9000 },
+                      new int[] { 10000 }
+                  });
+        }
+
+        [Test]
+        public void PartA()
+        {
+            // Arrange
+            var sut = CreateSut();
+
+            // Act
+            var result = sut.PartA();
+
+            // Assert
+            result.ShouldBe(24000);
+        }
+
+        [Test]
+        public void PartB()
+        {
+            // Arrange
+            var sut = CreateSut();
+
+            // Act
+            var result = sut.PartB();
+
+            // Assert
+            result.ShouldBe(45000);
+        }
+
+        private Day01 CreateSut() => new(loader.Object);
+    }
+}
---
+++ AdventOfCode/2022/Day01.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode._2022
+{
+    public class Day01 : CodeChallenge
+    {
+        public Day01(IInputLoader loader) : base(loader) { }
+
+        public override int Year => 2022;
+
+        public override int Day => 1;
+
+        public override long PartA()
+        {
+            var input = inputLoader.LoadListOfArrays<int>(inputLocation);
+
+            var elfCalories = new List<int>();
+            foreach (var elf in input)
+            {
+                elfCalories.Add(elf.Sum());
+            }
+
+            return elfCalories.Max();
+        }
+
+        public override long PartB()
+        {
+            var input = inputLoader.LoadListOfArrays<int>(inputLocation);
+
+            var elfCalories = new List<int>();
+            foreach (var elf in input)
+            {
+                elfCalories.Add(elf.Sum());
+            }
+
+            return elfCalories.OrderByDescending(x => x)
+                              .Take(3)
+                              .Sum();
+        }
+    }
+}
