9dec5eca932e0969faa102bf4e947ce38d1fca6c
Chris Caskey <cdcaskey@users.noreply.github.com>
2022-12-04 13:03:03 +0000

    Complete 2022 Day 4

---
+++ AdventOfCode.Tests/2022/Day04Tests.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using AdventOfCode._2022;
+using Moq;
+using NUnit.Framework;
+using Shouldly;
+
+namespace AdventOfCode.Tests._2022
+{
+    [TestFixture]
+    public class Day04Tests
+    {
+        private Mock<IInputLoader> loader;
+
+        [SetUp]
+        public void Setup()
+        {
+            loader = new Mock<IInputLoader>();
+            loader.Setup(x => x.LoadArray<string>(It.IsAny<string>(), It.IsAny<string>()))
+                  .Returns(
+                  [
+                      "2-4,6-8",
+                      "2-3,4-5",
+                      "5-7,7-9",
+                      "2-8,3-7",
+                      "6-6,4-6",
+                      "2-6,4-8",
+                  ]);
+        }
+
+        [Test]
+        public void PartA()
+        {
+            // Arrange
+            var sut = CreateSut();
+
+            // Act
+            var result = sut.PartA();
+
+            // Assert
+            result.ShouldBe(2);
+        }
+
+        [Test]
+        public void PartB()
+        {
+            // Arrange
+            var sut = CreateSut();
+
+            // Act
+            var result = sut.PartB();
+
+            // Assert
+            result.ShouldBe(4);
+        }
+
+        private Day04 CreateSut() => new(loader.Object);
+    }
+}
