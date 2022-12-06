79b980a8d84c85487e82077be7d5c0152ae23e2c
Chris Caskey <cdcaskey@users.noreply.github.com>
2022-12-06 10:38:20 +0000

    Complete 2022 Day 6

---
+++ AdventOfCode/2022/Day06.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode._2022
+{
+    public class Day06(IInputLoader loader) : CodeChallenge(loader)
+    {
+        public override object PartA() => CalculateSequenceStart(4);
+
+        public override object PartB() => CalculateSequenceStart(14);
+
+        private int CalculateSequenceStart(int requiredChars)
+        {
+            var input = inputLoader.LoadInput(InputLocation);
+
+            var chars = new Queue<char>();
+            for (var i = 0; i < requiredChars; i++)
+            {
+                chars.Enqueue(input[i]);
+            }
+
+            for (var i = requiredChars; i < input.Length; i++)
+            {
+                if (chars.Distinct().Count() == chars.Count)
+                {
+                    return i;
+                }
+
+                chars.Dequeue();
+                chars.Enqueue(input[i]);
+            }
+
+            throw new Exception("No Sequence Found");
+        }
+    }
+}
