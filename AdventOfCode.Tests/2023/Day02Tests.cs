ba57fa9f1d7fd109c31a1e482b519e0fe54f2044
Chris Caskey <cdcaskey@users.noreply.github.com>
2023-12-03 23:28:46 +0000

    Complete 2023 Day 2

---
+++ AdventOfCode.Tests/2023/Day02Tests.cs
@@ -0,0 +1,56 @@
+﻿using AdventOfCode._2023;
+using Moq;
+using NUnit.Framework;
+using Shouldly;
+
+namespace AdventOfCode.Tests._2023
+{
+    [TestFixture]
+    public class Day02Tests
+    {
+        private Mock<IInputLoader> loader;
+
+        [SetUp]
+        public void Setup()
+        {
+            loader = new Mock<IInputLoader>();
+            loader.Setup(x => x.LoadArray<string>(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(
+                [
+                    "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+                    "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+                    "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+                    "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+                    "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
+                ]);
+        }
+
+        [Test]
+        public void PartA()
+        {
+            // Arrange
+            var sut = CreateSut();
+
+            // Act
+            var result = sut.PartA();
+
+            // Assert
+            result.ShouldBe(8);
+        }
+
+        [Test]
+        public void PartB()
+        {
+            // Arrange
+            var sut = CreateSut();
+
+            // Act
+            var result = sut.PartB();
+
+            // Assert
+            result.ShouldBe(2286);
+        }
+
+        private Day02 CreateSut() => new(loader.Object);
+    }
+}
