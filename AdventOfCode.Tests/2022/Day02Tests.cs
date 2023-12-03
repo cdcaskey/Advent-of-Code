251499497ca140c32ce41ee36a0550e318f8a452
Chris Caskey <cdcaskey@users.noreply.github.com>
2023-12-03 22:18:24 +0000

    Complete 2022 Day 2

---
+++ AdventOfCode.Tests/2022/Day02Tests.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using AdventOfCode._2022;
+using Moq;
+using NUnit.Framework;
+using Shouldly;
+
+namespace AdventOfCode.Tests._2022
+{
+    [TestFixture]
+    public class Day02Tests
+    {
+        private Mock<IInputLoader> loader;
+
+        [SetUp]
+        public void Setup()
+        {
+            loader = new Mock<IInputLoader>();
+            loader.Setup(x => x.LoadArray<string>(It.IsAny<string>(), It.IsAny<string>()))
+                  .Returns(
+                  [
+                      "A Y",
+                      "B X",
+                      "C Z"
+                  ]);
+        }
+
+        [Test]
+        public void PartA()
+        {
+            // Arrange
+            var sut = CreateSut();
+
+            // Act
+            var result = sut.PartA();
+
+            // Assert
+            result.ShouldBe(15);
+        }
+
+        [Test]
+        public void PartB()
+        {
+            // Arrange
+            var sut = CreateSut();
+
+            // Act
+            var result = sut.PartB();
+
+            // Assert
+            result.ShouldBe(12);
+        }
+
+        private Day02 CreateSut() => new(loader.Object);
+    }
+}
---
+++ AdventOfCode/2022/Day02.cs
@@ -0,0 +1,129 @@
+﻿using System;
+
+namespace AdventOfCode._2022
+{
+    public class Day02(IInputLoader loader) : CodeChallenge(loader)
+    {
+        public override long PartA()
+        {
+            var input = inputLoader.LoadArray<string>(InputLocation);
+
+            var score = 0;
+            foreach (var line in input)
+            {
+                var opponent = (Option)(line[0] - 'A' + 1);
+                var player = (Option)(line[2] - 'X' + 1);
+
+                score += CalculateResult(player, opponent);
+            }
+
+            return score;
+        }
+
+        public override long PartB()
+        {
+            var input = inputLoader.LoadArray<string>(InputLocation);
+
+            var score = 0;
+            foreach (var line in input)
+            {
+                var opponent = (Option)(line[0] - 'A' + 1);
+                var result = (Result)(('X' - line[2]) * -1);
+
+                var player = CalculateOption(opponent, result);
+
+                score += (int)result * 3 + (int)player;
+            }
+
+            return score;
+        }
+
+        private static int CalculateResult(Option player, Option opponent)
+        {
+            Result result;
+
+            if (player == opponent)
+            {
+                result = Result.Draw;
+            }
+            else if (player == Option.Rock)
+            {
+                if (opponent == Option.Scissors)
+                {
+                    result = Result.Win;
+                }
+                else
+                {
+                    result = Result.Loss;
+                }
+            }
+            else if (player == Option.Paper)
+            {
+                if (opponent == Option.Rock)
+                {
+                    result = Result.Win;
+                }
+                else
+                {
+                    result = Result.Loss;
+                }
+            }
+            else
+            {
+                if (opponent == Option.Paper)
+                {
+                    result = Result.Win;
+                }
+                else
+                {
+                    result = Result.Loss;
+                }
+            }
+
+            return (int)result * 3 + (int)player;
+        }
+
+        private static Option CalculateOption(Option opponent, Result requiredResult)
+        {
+            if (requiredResult == Result.Draw)
+            {
+                return opponent;
+            }
+
+            if (requiredResult == Result.Win)
+            {
+                return opponent switch
+                {
+                    Option.Rock => Option.Paper,
+                    Option.Paper => Option.Scissors,
+                    Option.Scissors => Option.Rock,
+                    _ => throw new NotImplementedException()
+                };
+            }
+            else
+            {
+                return opponent switch
+                {
+                    Option.Rock => Option.Scissors,
+                    Option.Paper => Option.Rock,
+                    Option.Scissors => Option.Paper,
+                    _ => throw new NotImplementedException()
+                };
+            }
+        }
+
+        private enum Result
+        {
+            Win = 2,
+            Draw = 1,
+            Loss = 0
+        }
+
+        private enum Option
+        {
+            Rock = 1,
+            Paper = 2,
+            Scissors = 3
+        }
+    }
+}
