8e79b248cfa48d33830ac9b6237ede8fc3a1210f
Chris Caskey <cdcaskey@users.noreply.github.com>
2023-12-07 11:17:42 +0000

    Complete 2023 Day 7, Part A

---
+++ AdventOfCode.Tests/2023/Day07Tests.cs
@@ -0,0 +1,56 @@
+﻿using AdventOfCode._2023;
+using Moq;
+using NUnit.Framework;
+using Shouldly;
+
+namespace AdventOfCode.Tests._2023
+{
+    [TestFixture]
+    public class Day07Tests
+    {
+        private Mock<IInputLoader> loader;
+
+        [SetUp]
+        public void Setup()
+        {
+            loader = new Mock<IInputLoader>();
+            loader.Setup(x => x.LoadArray<string>(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(
+                [
+                    "32T3K 765",
+                    "T55J5 684",
+                    "KK677 28",
+                    "KTJJT 220",
+                    "QQQJA 483"
+                ]);
+        }
+
+        [Test]
+        public void PartA()
+        {
+            // Arrange
+            var sut = CreateSut();
+
+            // Act
+            var result = sut.PartA();
+
+            // Assert
+            result.ShouldBe(6440);
+        }
+
+        [Test]
+        public void PartB()
+        {
+            // Arrange
+            var sut = CreateSut();
+
+            // Act
+            var result = sut.PartB();
+
+            // Assert
+            result.ShouldBe(5905);
+        }
+
+        private Day07 CreateSut() => new(loader.Object);
+    }
+}
---
+++ AdventOfCode/2023/Day07.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode._2023
+{
+    public class Day07(IInputLoader loader) : CodeChallenge(loader)
+    {
+        private readonly Dictionary<char, int> cardStrengths = new()
+        {
+            { '2', 2 },
+            { '3', 3 },
+            { '4', 4 },
+            { '5', 5 },
+            { '6', 6 },
+            { '7', 7 },
+            { '8', 8 },
+            { '9', 9 },
+            { 'T', 10 },
+            { 'J', 11 },
+            { 'Q', 12 },
+            { 'K', 13 },
+            { 'A', 14 },
+        };
+
+        public override object PartA()
+        {
+            var input = inputLoader.LoadArray<string>(InputLocation);
+            var hands = ParseHands(input);
+            hands = OrderHands(hands);
+
+            var result = 0;
+            for (var i = 0; i < hands.Count; i++)
+            {
+                result += hands[i].Bid * (i + 1);
+            }
+
+            return result;
+        }
+
+        public override object PartB() => throw new NotImplementedException();
+
+        private List<Hand> ParseHands(string[] input)
+        {
+            var hands = new List<Hand>();
+            for (var i = 0; i < input.Length; i++)
+            {
+                var parts = input[i].Split(' ');
+                var cards = parts[0].ToCharArray();
+                hands.Add(new(cards, CalculateHandType(cards), int.Parse(parts[1])));
+            }
+
+            return hands;
+        }
+
+        private HandType CalculateHandType(char[] cards)
+        {
+            var distinctCards = cards.Distinct().ToArray();
+
+            switch(distinctCards.Length)
+            {
+                case 1:
+                    return HandType.FiveOfAKind;
+
+                case 2:
+                    if (cards.Count(x => x == distinctCards[0]) == 4 || cards.Count(x => x == distinctCards[1]) == 4)
+                    {
+                        return HandType.FourOfAKind;
+                    }
+                    else
+                    {
+                        return HandType.FullHouse;
+                    }
+
+                case 3:
+                    if (cards.Count(x => x == distinctCards[0]) == 3 ||
+                        cards.Count(x => x == distinctCards[1]) == 3 ||
+                        cards.Count(x => x == distinctCards[2]) == 3)
+                    {
+                        return HandType.ThreeOfAKind;
+                    }
+                    else
+                    {
+                        return HandType.TwoPair;
+                    }
+
+                case 4:
+                    return HandType.OnePair;
+            }
+
+            return HandType.HighCard;
+        }
+
+        private List<Hand> OrderHands(List<Hand> hands)
+            => hands.OrderBy(h => h.Type)
+                    .ThenBy(h => cardStrengths[h.Cards[0]])
+                    .ThenBy(h => cardStrengths[h.Cards[1]])
+                    .ThenBy(h => cardStrengths[h.Cards[2]])
+                    .ThenBy(h => cardStrengths[h.Cards[3]])
+                    .ThenBy(h => cardStrengths[h.Cards[4]])
+                    .ToList();
+
+        private record Hand(char[] Cards, HandType Type, int Bid);
+
+        private enum HandType
+        {
+            HighCard = 0,
+            OnePair,
+            TwoPair,
+            ThreeOfAKind,
+            FullHouse,
+            FourOfAKind,
+            FiveOfAKind
+        }
+    }
+}
